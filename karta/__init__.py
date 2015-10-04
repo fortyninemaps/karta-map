3a1a3d06656b94e1e97d9a9014f189a4100cd9b7
Nat Wilson <njwilson23@gmail.com>
2015-10-04 13:42:45 -0700

    add setup script and merge with karta namespace

---
+++ karta/__init__.py
@@ -0,0 +1,5 @@
+import pkgutil
+__path__ = pkgutil.extend_path(__path__, __name__)
+
+import mapping
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='karta_map',
+        version='0.0a1',
+        packages=find_packages(),
+        author="Nat Wilson",
+        description="Mapping utilities for karta",
+        install_requires=['numpy>=1.7',
+                          'scipy>=0.12',
+                          'karta>=0.5.0',
+                          'matplotlib>=1.5',
+                          'typing'],
+        license = "MIT License",
+      )
+
+
